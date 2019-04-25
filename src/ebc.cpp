efe69c5d99062720f795a32040800cd5e1c64334
synnick <nskelsey@gmail.com>
2019-04-25 19:03:53 +0200

    Move towards pcap file processing

--- src/ebc.cpp
+++ src/ebc.cpp
@@ -1,19 +1,36 @@
 #include <cstring>
 #include <stdio.h>
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
 
-#include <file.h>
-#include <cfile.h>
+#include "file.h"
+#include "cfile.h"
 #include <capture_info.h>
-#include <capchild/capture_session.h>
+#include <capture_opts.h>
 #include <capchild/capture_sync.h>
+#include <capchild/capture_session.h>
 #include <caputils/capture-pcap-util.h>
 #include <epan/addr_resolv.h>
+#include <epan/conversation_table.h>
 #include <epan/epan.h>
 #include <epan/funnel.h>
-#include <pcap.h>
+#include <epan/prefs.h>
+#include <epan/print_stream.h>
+#include <epan/register.h>
+#include <epan/rtd_table.h>
+#include <epan/srt_table.h>
+#include <epan/stat_tap_ui.h>
+#include <epan/timestamp.h>
+#include "pcap.h"
+#include "log.h"
+#include "extcap.h"
 #include <ui/capture.h>
 #include <ui/ws_ui_util.h>
+#include <ui/taps.h>
+#include <ui/cli/tshark-tap.h>
+#include <ui/filter_files.h>
+#include <ui/dissect_opts.h>
 #include <version_info.h>
 
 #include <Corrade/Containers/Array.h>
@@ -24,7 +41,15 @@
 #include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/Platform/Sdl2Application.h>
 
+/* Exit codes */
+#define INIT_FAILED 2
+#define INVALID_FILE 2
+#define INVALID_OPTION 2
+
 capture_file cfile;
+
+static capture_options global_capture_opts;
+static capture_session global_capture_session;
 
 void pipe_input_set_handler(gint source, gpointer user_data, ws_process_id *child_process, pipe_input_cb_t input_cb);
 void capture_input_new_packets(capture_session *cap_session, int to_read);
@@ -60,7 +85,7 @@ cf_status_t cf_open(capture_file *cf, const char *fname, unsigned int type, gboo
   wtap  *wth;
   gchar *err_info;
 
-  bool perform_two_pass_analysis = true;
+  bool perform_two_pass_analysis = false;
   wth = wtap_open_offline(fname, type, err, &err_info, perform_two_pass_analysis);
   if (wth == NULL)
     goto fail;
@@ -107,11 +132,45 @@ fail:
   return CF_ERROR;
 }
 
+// NOTE robbed from capture_sync.c
+/*
+void capture_session_init(capture_session *cap_session, capture_file *cf)
+{
+    cap_session->cf                              = cf;
+    cap_session->fork_child                      = WS_INVALID_PID;
+    cap_session->owner                           = getuid();
+    cap_session->group                           = getgid();
+    cap_session->state                           = CAPTURE_STOPPED;
+    cap_session->count                           = 0;
+    cap_session->session_started                 = FALSE;
+}
+*/
+
+
+static void
+tshark_log_handler (const gchar *log_domain, GLogLevelFlags log_level,
+    const gchar *message, gpointer user_data)
+{
+
+  if ((log_level & (G_LOG_LEVEL_DEBUG | G_LOG_LEVEL_INFO))) {
+    const gchar *domains = g_getenv("G_MESSAGES_DEBUG");
+    if (domains == NULL ||
+       (strcmp(domains, "all") != 0 && !strstr(domains, log_domain))) {
+      return;
+    }
+  }
+
+  g_log_default_handler(log_domain, log_level, message, user_data);
+}
+
+
+
 static void
 get_tshark_compiled_version_info(GString *str)
 {
   /* Capture libraries */
   get_compiled_caplibs_version(str);
+
 }
 
 namespace Magnum {
@@ -126,6 +185,20 @@ class Ebc: public Platform::Application
 };
 
 Ebc::Ebc(const Arguments& arguments): Platform::Application{arguments, Configuration{}.setTitle("EvenBetterCap")} {
+
+    volatile int exit_status = EXIT_SUCCESS;
+
+    e_prefs             *prefs_p;
+    gboolean prefs_loaded = FALSE;
+    print_format_e print_format = PR_FMT_TEXT;
+    print_stream_t* print_stream = NULL;
+    const char* delimiter_char;
+
+    const char *output_file_name;
+    output_fields_t* output_fields;
+    const gchar* cf_name;
+    volatile int in_file_type = WTAP_TYPE_AUTO;
+    int err;
 
     Magnum::Utility::Arguments args;
     args.addArgument("filename").setHelp("filename","the pcap file to process")
@@ -152,58 +225,110 @@ Ebc::Ebc(const Arguments& arguments): Platform::Application{arguments, Configura
     g_string_free(comp_info_str, TRUE);
     g_string_free(runtime_info_str, TRUE);
 
-    std::cout << "hello other world!" << std::endl;
+    int log_flags =
+                    G_LOG_LEVEL_ERROR|
+                    G_LOG_LEVEL_CRITICAL|
+                    G_LOG_LEVEL_WARNING|
+                    G_LOG_LEVEL_MESSAGE|
+                    G_LOG_LEVEL_INFO|
+                    G_LOG_LEVEL_DEBUG|
+                    G_LOG_FLAG_FATAL|G_LOG_FLAG_RECURSION;
 
-    epan_load_settings();
-    write_prefs(NULL);
+    g_log_set_handler(NULL,
+                    (GLogLevelFlags)log_flags,
+                    tshark_log_handler, NULL /* user_data */);
+    g_log_set_handler(LOG_DOMAIN_MAIN,
+                    (GLogLevelFlags)log_flags,
+                    tshark_log_handler, NULL /* user_data */);
+
+    g_log_set_handler(LOG_DOMAIN_CAPTURE,
+                    (GLogLevelFlags)log_flags,
+                    tshark_log_handler, NULL /* user_data */);
+    g_log_set_handler(LOG_DOMAIN_CAPTURE_CHILD,
+                    (GLogLevelFlags)log_flags,
+                    tshark_log_handler, NULL /* user_data */);
+
+    capture_opts_init(&global_capture_opts);
+    //capture_session_init(&global_capture_session, &cfile);
+
+    timestamp_set_type(TS_RELATIVE);
+    timestamp_set_precision(TS_PREC_AUTO);
+    timestamp_set_seconds_type(TS_SECONDS_DEFAULT);
+
+    std::cout << "YYYY" << std::endl;
+    wtap_init(TRUE);
+    std::cout << "ZZZZ" << std::endl;
+    if (!epan_init(register_all_protocols, register_all_protocol_handoffs, NULL,
+                   NULL)) {
+
+      exit_status = INIT_FAILED;
+      goto clean_exit;
+    }
+    std::cout << "XXXX" << std::endl;
+
+    // Register all tap listeners.
+    extcap_register_preferences();
+    for (tap_reg_t *t = tap_reg_listener; t->cb_func != NULL; t++) {
+      t->cb_func();
+    }
+
+    /*
+    conversation_table_set_gui_info(init_iousers);
+    hostlist_table_set_gui_info(init_hostlists);
+    srt_table_iterate_tables(register_srt_tables, NULL);
+    rtd_table_iterate_tables(register_rtd_tables, NULL);
+    stat_tap_iterate_tables(register_simple_stat_tables, NULL);
+    */
 
     std::cout << "hello third world!" << std::endl;
 
+    /* Load libwireshark settings from the current profile. */
+    prefs_p = epan_load_settings();
+    //write_prefs(NULL);
+    prefs_loaded = TRUE;
 
-    char errbuf[PCAP_ERRBUF_SIZE] = "N/A";
-    // TODO Read content inside of an epan scope for classification.
+    read_filter_list(CFILTER_LIST);
 
-    std::cout << args.value("filename") << std::endl;
+    cap_file_init(&cfile);
 
-    pcap_t *handle = pcap_open_offline(args.value("filename").c_str(), errbuf);
-    if (handle == NULL) {
-        fprintf(stderr, "Failed to create interface: %s\n", errbuf);
-        std::exit(1);
+
+    /* Print format defaults to this. */
+    print_format = PR_FMT_TEXT;
+    delimiter_char = " ";
+
+    output_fields = output_fields_new();
+
+    output_file_name = args.value("filename").c_str();
+    cf_name = g_strdup(output_file_name);
+
+    prefs_apply_all();
+
+    //start_exportobjects();
+
+    timestamp_set_type(global_dissect_options.time_format);
+
+    if (!setup_enabled_and_disabled_protocols()) {
+      exit_status = INVALID_OPTION;
+      goto clean_exit;
     }
 
-    int linktype = pcap_datalink(handle);
-    std::cout << "Link type ";
-    switch(linktype) {
-        case DLT_EN10MB:
-            std::cout << "Ether 802.3" << std::endl;
-            break;
-        case DLT_LINUX_SLL:
-            std::cout << "Linux SLL" << std::endl;
-            std::exit(1);
-            break;
-        case DLT_IEEE802_11:
-            std::cout << "Wifi 802.11" << std::endl;
-            std::exit(1);
-            break;
-        default:
-            std::cout << "Type not handled" << std::endl;
-            std::exit(1);
+    print_stream = print_stream_text_stdio_new(stdout);
+
+    if (cf_open(&cfile, cf_name, in_file_type, FALSE, &err) != CF_OK) {
+      epan_cleanup();
+      extcap_cleanup();
+      exit_status = INVALID_FILE;
+      goto clean_exit;
     }
 
-    u_char b[9000];
-    pcap_loop(handle, 10, packet_handler, b);
 
-    wtap_init(FALSE);
-
-    if (!epan_init(register_all_protocols, register_all_protocol_handoffs, NULL, NULL)) {
-        std::cout << "Epan init failed" << std::endl;
-        std::exit(1);
-    }
+clean_exit:
+    capture_opts_cleanup(&global_capture_opts);
 
     epan_cleanup();
     wtap_cleanup();
-    pcap_close(handle);
-
+    //cf_close(&cfile);
+    std::exit(exit_status);
 }
 
 void Ebc::drawEvent() {
